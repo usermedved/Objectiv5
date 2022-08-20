a1b4c585091c90a5d1f37ae88b2d3ca24e8243d7
Sergei Medvedev <clip1988@yandex.ru>
2022-08-20 21:15:33 +0300

    Objectiv 34 project

---
+++ Objectiv34/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] result = new int[10];
+int evenNumber = 0;
+for (int i = 0; i < result.Length; i++)
+{
+    result[i] = new Random().Next(100, 1000);
+    if(result[i] % 2 ==0)
+    {
+        evenNumber++;
+    } 
+   }
+Console.WriteLine($"Массив: [{String.Join(",", result)}]");   
+Console.WriteLine($"Колличество четных чисел в массиве: {evenNumber}");

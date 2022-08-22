bb7dd218c988ed3744b400d95d3020078a00581f
Sergei Medvedev <clip1988@yandex.ru>
2022-08-22 22:01:59 +0300

    Objectiv 36 project

--- Objectiv34/Program.cs
+++ Objectiv34/Program.cs
@@ -1,12 +1,12 @@
-﻿int[] result = new int[10];
+﻿int[] array = new int[10];
 int evenNumber = 0;
-for (int i = 0; i < result.Length; i++)
+for (int i = 0; i < array.Length; i++)
 {
-    result[i] = new Random().Next(100, 1000);
-    if(result[i] % 2 ==0)
+    array[i] = new Random().Next(100, 1000);
+    if(array[i] % 2 ==0)
     {
         evenNumber++;
     } 
    }
-Console.WriteLine($"Массив: [{String.Join(",", result)}]");   
+Console.WriteLine($"Массив: [{String.Join(",", array)}]");   
 Console.WriteLine($"Колличество четных чисел в массиве: {evenNumber}");
---
+++ Objectiv36/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] array = new int[10];
+double oddElement = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100);
+    if(i % 2 == 1) oddElement += array[i];
+     
+}
+Console.WriteLine($"Массив: [{String.Join(",", array)}]");   
+Console.WriteLine($"Колличество четных чисел в массиве: {oddElement}");

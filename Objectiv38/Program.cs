69d814489d469918e5fd9c5be62ca1e635044f0e
Sergei Medvedev <clip1988@yandex.ru>
2022-08-23 21:42:12 +0300

    Objectiv 38 project

---
+++ Objectiv38/Program.cs
@@ -0,0 +1,18 @@
+﻿double[] differenceArrayElement = new double[5];
+
+for (int i = 0; i < differenceArrayElement.Length; i++)
+{
+    differenceArrayElement[i] = new Random().Next(100);
+}
+Console.WriteLine($"Массив: [{String.Join(",", differenceArrayElement)}]");
+
+double max = differenceArrayElement[0];
+double min = differenceArrayElement[0];
+
+for (int i = 0; i < differenceArrayElement.Length; i++)
+{
+    if(differenceArrayElement[i] > max) max = differenceArrayElement[i];
+    if(differenceArrayElement[i] < min) min = differenceArrayElement[i];
+}
+double difference = max - min;
+Console.WriteLine($"Разница между минимальным {min} и максимальным {max} элементом массива: {difference}");
